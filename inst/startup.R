384eb636e2ca9df5d09d6cbed3a0aedca6e03386
Yilong Zhang <yilong.zhang@merck.com>
2021-08-08 16:29:10 -0400

    add startup file

--- inst/startup.R
+++ inst/startup.R
@@ -4,8 +4,8 @@
 
 R_version <- "4.0.1"																                  # set up project R version
 snapshot  <- "2021-08-06" 									                          # set up snapshot date
-repos     <- paste0("https://mran.microsoft.com/snapshot/", snapshot) # set up repository based on snapshot
-home      <- normalizePath(".")                                       # set up home directory
+repos     <- paste0("https://mran.microsoft.com/snapshot/", snapshot)  # set up repository based on snapshot
+home      <- dirname(rprojroot::find_package_root_file("DESCRIPTION")) # set up home directory
 
 # A&R folder path (Do not edit information below)
 path <- list(
